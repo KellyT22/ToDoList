ca65f1b5ffa301ca6b9d5e9db68a05330a5eabdb
Kelly Tu <kelly22004t@gmail.com>
2020-06-23 13:39:28 -0500

    Initial Commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Kelly Tu on 6/22/20.
+//  Copyright © 2020 Kelly Tu. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kelly Tu on 6/22/20.
+//  Copyright © 2020 Kelly Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
